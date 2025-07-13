02734d470011bbdbd29c8c08d5877649396c410a
GoogleFrog <googl3frog@gmail.com>
2025-07-13 18:10:05 +1000

    Make replay graph parser.

---
+++ replayPageParser.py
@@ -0,0 +1,208 @@
+from datetime import datetime, timedelta
+import heapq
+import math
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+
+def ProcessBlock(lines, processed, filterOut):
+	if len(lines[3].split()) < 2:
+		print(lines)
+		return
+	if lines[6].split()[2] == "seconds":
+		return
+	title = lines[0]
+	if title in filterOut:
+		return
+	players = int(lines[3].split()[1])
+	specs = int(lines[4].split()[1])
+	start = lines[5].split()[1] + ' ' + lines[5].split()[2] + ' ' + lines[5].split()[3]
+	start = datetime.strptime(start, "%m/%d/%Y %I:%M:%S %p")
+	duration = int(lines[6].split()[1])
+
+	if title not in processed:
+		processed[title] = []
+	processed[title].append({
+		"players" : players,
+		"specs" : specs,
+		"start" : start,
+		"end" : start + timedelta(minutes=duration),
+		"duration" : duration,
+	})
+
+
+def SortBattles(a):
+	return a["start"]
+
+
+def MakeBattleList(processed):
+	print('{},{},{},{},{}'.format("Room", "Players", "Time", "Duration", "Spectators"))
+	for title, battles in processed.items():
+		battles.sort(key=SortBattles)
+		for data in battles:
+			print('{},{},{},{},{}'.format(
+				title, data["players"], data["start"].strftime("%H:%M"),
+				data["duration"], data["specs"]))
+
+
+def PlotTimeline(times, data, minuteScale):
+	data = data.copy()
+	del data["specs"]
+
+	labels = list(data.keys())[::-1]
+	values = [data[label] for label in labels]
+
+	fig, ax = plt.subplots(figsize=(12, 6))
+
+	ax.stackplot(times, values, labels=labels)
+	ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M %d/%m'))
+	ax.xaxis.set_major_locator(mdates.HourLocator(interval=24))
+
+	plt.legend(loc='upper left')
+	plt.title('Maximum players in game by {}-minute period {} to {}'.format(minuteScale, times[0], times[-1]))
+	plt.xlabel('Time')
+	plt.ylabel('Players')
+
+	plt.xticks(rotation=45)
+	plt.tight_layout()
+	plt.show()
+
+
+def PrintTimeline(minTime, maxTime, minuteScale, playerLists):
+	print("Time," + ",".join(playerLists.keys()))
+	time = minTime
+	index = 0
+	while time < maxTime:
+		str = time.strftime("%H:%M")
+		for count in playerLists.values():
+			str = str + ",{}".format(count[index])
+		print(str)
+		time = time + timedelta(minutes=minuteScale)
+		index = index + 1
+
+
+def MakeAverageCounts(processed, minTime, step, times, mostBattles):
+	playerLists = {}
+	totalSpecs = False
+	for title in mostBattles:
+		players = [0] * len(times)
+		specs = [0] * len(times)
+		time = minTime
+		index = 0
+		battles = processed[title]
+		for data in battles:
+			while time < data["start"] - step:
+				time += step
+				index += 1
+			while time < data["end"] - step:
+				if time < data["start"]:
+					prop = (min(time + step, data["end"]) - data["start"]) / step
+					players[index] += data["players"] * prop
+					specs[index] += data["specs"] * prop
+				else:
+					players[index] += data["players"]
+					specs[index] += data["specs"]
+				time += step
+				index += 1
+			prop = (data["end"] - max(time, data["start"])) / step
+			players[index] += data["players"] * prop
+			specs[index] += data["specs"] * prop
+		
+		playerLists[title] = players
+		if totalSpecs is False:
+			totalSpecs = specs
+		else:
+			totalSpecs = [a + b for a, b in zip(specs, totalSpecs)]
+	playerLists["specs"] = totalSpecs
+	return playerLists
+
+
+def MakeMaximumCounts(processed, minTime, step, times, mostBattles):
+	playerLists = {}
+	totalSpecs = False
+	for title in mostBattles:
+		players = [0] * len(times)
+		specs = [0] * len(times)
+		time = minTime
+		index = 0
+		battles = processed[title]
+		for data in battles:
+			while time < data["start"] - step:
+				time += step
+				index += 1
+			while time < data["end"]:
+				players[index] = max(players[index], data["players"])
+				specs[index] = max(specs[index], data["specs"])
+				time += step
+				index += 1
+		playerLists[title] = players
+		if totalSpecs is False:
+			totalSpecs = specs
+		else:
+			totalSpecs = [a + b for a, b in zip(specs, totalSpecs)]
+	playerLists["specs"] = totalSpecs
+	return playerLists
+
+
+def MakeTimeline(processed, trackCount, minuteScale):
+	minTime = False
+	maxTime = False
+	battleCount = {k : len(v) for k, v in processed.items()}
+	mostBattles = heapq.nlargest(trackCount, battleCount, key=battleCount.get)
+	print(processed.keys())
+
+	for title in mostBattles:
+		battles = processed[title]
+		for data in battles:
+			if minTime is False or minTime > data["start"]:
+				minTime = data["start"]
+			if maxTime is False or maxTime < data["end"]:
+				maxTime = data["end"]
+	
+	minTime = minTime.replace(minute=math.floor(minTime.minute/minuteScale)*minuteScale, second=0)
+	maxTime = maxTime.replace(minute=math.floor(maxTime.minute/minuteScale)*minuteScale, second=0)
+	maxTime = maxTime + timedelta(minutes=minuteScale)
+	
+	times = []
+	time = minTime
+	step = timedelta(minutes=minuteScale)
+	while time < maxTime:
+		times.append(time)
+		time = time + step
+
+	#playerLists = MakeAverageCounts(processed, minTime, step, times, mostBattles)
+	playerLists = MakeMaximumCounts(processed, minTime, step, times, mostBattles)
+
+	PlotTimeline(times, playerLists, minuteScale)
+	#PrintTimeline(minTime, maxTime, minuteScale, playerLists)
+
+
+file = "paste13_06_25_to_13_07_25.txt"
+trackCount = 5
+minuteScale = 20
+
+filterOut = [
+	"[A] Pro 1v1 Host",
+	"[A] COOP vs AI",
+	"[A] Future Wars Coop",
+	"[A] Units Level Up",
+	"[A] Arena Mod",
+]
+
+with open(file) as file:
+	lines = []
+	processed = {}
+	for line in file:
+		line = line.rstrip()
+		if line != "":
+			lines.append(line)
+			if 'Duration' in line:
+				ProcessBlock(lines, processed, filterOut)
+				lines = []
+	
+	for title, battles in processed.items():
+		battles.sort(key=SortBattles)
+	
+	#MakeBattleList(processed)
+	MakeTimeline(processed, trackCount, minuteScale)
